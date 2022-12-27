918721a5e74e0d9c9ea3c75e1cb64a30c85c93de
Alex <anonymousaaaa41414141@gmail.com>
2022-12-27 14:56:27 +0800

    Remove unused code

--- components/pages/HomePage/components/Banner/index.js
+++ components/pages/HomePage/components/Banner/index.js
@@ -1,31 +1,31 @@
-import style from "./Banner.module.css";
+import styles from "./Banner.module.css";
 
 
 function join(...array) {
     return array.join(" ");
 }
 
-export const Banner = ({ languagePack, language, isLoading, setIsLoading }) => {
+export default function Banner({ languagePack, language, isLoading, setIsLoading }) {
     return (
-        <div className={style.banner_container}>
-            <div className={style.banner}>
-                <div className={style.banner_video}>
-                    <video autoPlay muted loop onLoadedData={() => setIsLoading(false)}>
+        <div className={styles.banner_container}>
+            <div className={styles.banner}>
+                <div className={styles.banner_video}>
+                    <video autoPlay muted loop>
                         <source src="/Fauna.mp4" type="video/mp4" />
                     </video>
                 </div>
-                <div className={style.banner_filter}></div>
+                <div className={styles.banner_filter}></div>
             </div>
-            <div className={style.learnmore_container}>
-                <div className={style.learnmore}>
-                    <h1 className={style.learnmore_title}>
-                        Ceres Fauna Christmas ASMR
+            <div className={styles.learnmore_container}>
+                <div className={styles.learnmore}>
+                    <h1 className={styles.learnmore_title}>
+                        Ceres Fauna <label className={styles.title_split}>|</label> Christmas ASMR
                     </h1>
-                    <button className={style.learnmore_button} title={languagePack["Learn More"][language]}>
-                        <span className={style.learnmore_button_circle} aria-hidden="true">
-                            <span className={join(style.learnmore_button_circle_icon, style.learnmore_button_circle_icon_arrow)}></span>
+                    <button className={styles.learnmore_button} title={languagePack["Learn More"][language]}>
+                        <span className={styles.learnmore_button_circle} aria-hidden="true">
+                            <span className={join(styles.learnmore_button_circle_icon, styles.learnmore_button_circle_icon_arrow)}></span>
                         </span>
-                        <span className={style.learnmore_button_text}>{languagePack["Learn More"][language]}</span>
+                        <span className={styles.learnmore_button_text}>{languagePack["Learn More"][language]}</span>
                     </button>
                 </div>
             </div>
