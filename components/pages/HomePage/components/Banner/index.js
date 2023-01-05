46bb10c9f0d148b79c952d873f5c839cf21769dd
Alex <anonymousaaaa41414141@gmail.com>
2023-01-05 15:40:49 +0800

    Increase the height of banner
    Changes
    -Increase the height of banner to full view height
    -Improve the align behavier of "learnmore" section
    
    Fix
    -"learnmore title" will now display the correct font family

--- components/pages/HomePage/components/Banner/index.js
+++ components/pages/HomePage/components/Banner/index.js
@@ -7,28 +7,30 @@ function join(...array) {
 
 export default function Banner({ languagePack, language, isLoading, setIsLoading }) {
     return (
-        <div className={styles.banner_container}>
-            <div className={styles.banner}>
-                <div className={styles.banner_video}>
-                    <video autoPlay muted loop>
-                        <source src="/Fauna.mp4" type="video/mp4" />
-                    </video>
+        <>
+            <div className={join(styles.banner_container, "noselect")}>
+                <div className={styles.banner}>
+                    <div className={styles.banner_video}>
+                        <video autoPlay muted loop onPlaying={() => setIsLoading(false)} >
+                            <source src="/Fauna.mp4" type="video/mp4" />
+                        </video>
+                    </div>
+                    <div className={styles.banner_filter}></div>
                 </div>
-                <div className={styles.banner_filter}></div>
-            </div>
-            <div className={styles.learnmore_container}>
-                <div className={styles.learnmore}>
-                    <h1 className={styles.learnmore_title}>
-                        Ceres Fauna <label className={styles.title_split}>|</label> Christmas ASMR
-                    </h1>
-                    <button className={styles.learnmore_button} title={languagePack["Learn More"][language]}>
-                        <span className={styles.learnmore_button_circle} aria-hidden="true">
-                            <span className={join(styles.learnmore_button_circle_icon, styles.learnmore_button_circle_icon_arrow)}></span>
-                        </span>
-                        <span className={styles.learnmore_button_text}>{languagePack["Learn More"][language]}</span>
-                    </button>
+                <div className={styles.learnmore_container}>
+                    <div className={styles.learnmore}>
+                        <h1 className={styles.learnmore_title}>
+                            Ceres Fauna <label className={styles.title_split}>|</label> Christmas ASMR
+                        </h1>
+                        <button className={styles.learnmore_button} title={languagePack["Learn More"][language]}>
+                            <span className={styles.learnmore_button_circle} aria-hidden="true">
+                                <span className={join(styles.learnmore_button_circle_icon, styles.learnmore_button_circle_icon_arrow)}></span>
+                            </span>
+                            <span className={styles.learnmore_button_text}>{languagePack["Learn More"][language]}</span>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
