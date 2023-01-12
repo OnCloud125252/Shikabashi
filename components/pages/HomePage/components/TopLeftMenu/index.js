3276ecfd6b7ecd41693cc7e8dd9e5a848daf6a54
Alex <anonymousaaaa41414141@gmail.com>
2023-01-12 20:13:43 +0800

    Add logo and navigate bar
    Changes
    -Improve structure of languagepack
    -Fix bug that cause content inside triangle not display properly

--- components/pages/HomePage/components/TopLeftMenu/index.js
+++ components/pages/HomePage/components/TopLeftMenu/index.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
+
+import { languagePack } from "../../../../languagePack";
 import styles from "./TopLeftMenu.module.css";
 
 
@@ -7,7 +9,7 @@ function join(...array) {
     return array.join(" ");
 }
 
-export default function TopLeftMenu({ languagePack, language, setLanguage, isLoading, setIsLoading }) {
+export default function TopLeftMenu({ language, setLanguage, isLoading, setIsLoading }) {
     const [windowDimensions, setWindowDimensions] = useState(0);
 
     function getWindowDimensions(window) {
@@ -37,12 +39,22 @@ export default function TopLeftMenu({ languagePack, language, setLanguage, isLoa
     return (
         <>
             <div className={join(styles.triangle)} style={{
-                "opacity": isLoading ? "1" : "0.4",
-                "--rotate-angle": isLoading ? "0deg" : `-${Math.atan(130 / windowDimensions.width)}rad`,
-                "height": isLoading ? "100vh" : "120px",
+                "backgroundColor": isLoading ? "rgba(0, 0, 0, 1)" : "rgba(0, 0, 0, 0.4)",
+                "--rotate-angle": isLoading ? "0deg" : `-${Math.atan(100 / windowDimensions.width)}rad`,
+                "height": isLoading ? "100vh" : "90px",
                 "zIndex": isLoading ? "999" : "1",
             }}>
-                <Image src="/logo_withName.svg" alt="Shikabashi logo" className={styles.logo} width={56} height={80} />
+                <nav className={join(styles.navbar, "noselect")}>
+
+                    <div className={styles.menu}>
+                        <Image src="/logo_noName.svg" alt="Shikabashi logo" className={styles.logo} width={600} height={600} />
+                        <p className={styles.item}>{languagePack.pages.HomePage.components.TopLeftMenu["Creation Hall"][language]}</p>
+                        <p className={styles.item}>{languagePack.pages.HomePage.components.TopLeftMenu["About Us"][language]}</p>
+                        <p className={styles.item}>{languagePack.pages.HomePage.components.TopLeftMenu["Frequently Asked"][language]}</p>
+                        <p className={styles.item}>{languagePack.pages.HomePage.components.TopLeftMenu["Contact Us"][language]}</p>
+
+                    </div>
+                </nav>
             </div>
         </>
     );
