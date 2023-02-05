a7d42b716f5373b4044e32d93286b230e35497fa
Alex Liao <anonymousaaaa41414141@gmail.com>
2023-02-06 02:12:03 +0800

    Fix display problem of title in content

--- components/pages/HomePage/components/Content/index.js
+++ components/pages/HomePage/components/Content/index.js
@@ -74,6 +74,9 @@ export default function Content({ parameters }) {
                             <div className={styles.card_description}>How do you drown a hipster?<br />Throw him in the mainstream.</div>
                         </div>
                     </div>
+                </div>
+                <div className={styles.title}><span>{languagePack.pages.HomePage.components.Content["Our Works"][language]}</span></div>
+                <div className={styles.card_container}>
                     <div className={styles.card_16x9_empty} />
                     <div className={styles.card_10x18}>
                         <Image src="/92765703.jpg" alt="image 10x18" width={2200} height={1555} className={styles.image} />
