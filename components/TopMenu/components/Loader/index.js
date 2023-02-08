1c8a1d88ae5a0d0f5c687dea3bb677598ee19bbd
Alex <anonymousaaaa41414141@gmail.com>
2023-02-08 18:53:30 +0800

    Add a loader to display when loading banner

---
+++ components/TopMenu/components/Loader/index.js
@@ -0,0 +1,31 @@
+import styles from "./Loader.module.css";
+
+
+export default function Loader({ isLoading }) {
+    return (
+        <>
+            <div className={styles.mosaicLoader} style={{ "opacity": isLoading ? 1 : 0 }}>
+                <div className={join(styles.cell, styles.d_0)} />
+                <div className={join(styles.cell, styles.d_1)} />
+                <div className={join(styles.cell, styles.d_2)} />
+                <div className={join(styles.cell, styles.d_3)} />
+                <div className={join(styles.cell, styles.d_1)} />
+                <div className={join(styles.cell, styles.d_2)} />
+                <div className={join(styles.cell, styles.d_3)} />
+                <div className={join(styles.cell, styles.d_4)} />
+                <div className={join(styles.cell, styles.d_2)} />
+                <div className={join(styles.cell, styles.d_3)} />
+                <div className={join(styles.cell, styles.d_4)} />
+                <div className={join(styles.cell, styles.d_5)} />
+                <div className={join(styles.cell, styles.d_3)} />
+                <div className={join(styles.cell, styles.d_4)} />
+                <div className={join(styles.cell, styles.d_5)} />
+                <div className={join(styles.cell, styles.d_6)} />
+            </div>
+        </>
+    );
+}
+
+function join(...array) {
+    return array.join(" ");
+}
