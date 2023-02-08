13d0241f93d52b905fea6c84be78427bcaad81f9
Alex <anonymousaaaa41414141@gmail.com>
2023-02-08 18:52:51 +0800

    Change layout of language changer

---
+++ components/TopMenu/components/LanguageChanger/index.js
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import { useState } from "react";
+import { setCookie } from "cookies-next";
+
+import { languagePack } from "../../../languagePack";
+import globe from "./globe.png";
+import styles from "./LanguageChanger.module.css";
+
+
+export default function LanguageChanger({ language, setLanguage }) {
+    const languageList = Object.keys(languagePack.languages);
+
+    const [showDropdown, setShowDropdown] = useState(false);
+
+
+    return (
+        <>
+            <div className={styles.languageChanger}>
+                <div className={styles.selectedLanguage} onClick={() => setShowDropdown(!showDropdown)}>
+                    {languagePack.languages[language].displayName}
+                    {/* <div className={styles.globe}>🌐</div> */}
+                    <Image className={styles.globe} src={globe} alt="globe image" width={24} height={24} />
+                    {/* {showDropdown ? <div className={styles.languageChanger_arrowUp} /> : <div className={styles.languageChanger_arrowDown} />} */}
+                </div>
+                {
+                    showDropdown ? (
+                        languageList
+                            .filter((languageCode) => languageCode !== language)
+                            .map((languageCode, index) =>
+                                <div key={index} className={styles.languageDropdownOption} onClick={() => {
+                                    switchLanguageTo(languageCode);
+                                    setShowDropdown(false);
+                                }} style={{ "fontFamily": languagePack.languages[languageCode].fontFamily }}>
+                                    {languagePack.languages[languageCode].displayName}
+                                </div>
+                            )
+                    ) : <></>
+                }
+            </div>
+        </>
+    );
+
+    function switchLanguageTo(languageCode) {
+        setLanguage(languageCode);
+        setCookie("language", languageCode);
+    }
+}
